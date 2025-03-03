4fcd1bf5d32066a492fa7e09a22080b5ce834825
MaciejM <39690589+siasty@users.noreply.github.com>
2025-03-03 12:12:21 +0100

    Add files via upload

---
+++ ClassInterfaces/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassInterfaces
+{
+    public interface IProjectFunctions
+    {
+        void SomeFunction();
+        string GetData(int id);
+    }
+
+    public interface IDllFunctions
+    {
+        void DllMethod();
+        void SetProjectReference(IProjectFunctions projectFunctions);
+    }
+}
