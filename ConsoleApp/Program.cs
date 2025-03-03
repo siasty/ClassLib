03d310b1978c2acca7c9571397e494d01be40da1
MaciejM <39690589+siasty@users.noreply.github.com>
2025-03-03 12:14:55 +0100

    Add files via upload

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassInterfaces;
+using ConsoleApp;
+using DLLProject;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        IDllFunctions dll = new dllClass();
+        var project = new projectClass(dll);
+        project.InitializeDll();
+
+        Console.WriteLine("Starting test:");
+        project.SomeFunction();
+        dll.DllMethod();
+        Console.WriteLine($"DLL getting project data: {((dllClass)dll).GetProjectData(42)}");
+        project.CallDllSeparately();
+    }
+}
---
+++ ConsoleApp/projectClass.cs
@@ -0,0 +1,39 @@
+﻿using ClassInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class projectClass : IProjectFunctions
+    {
+        private readonly IDllFunctions _dllFunctions;
+
+        public projectClass(IDllFunctions dllFunctions)
+        {
+            _dllFunctions = dllFunctions;
+        }
+
+        public void SomeFunction()
+        {
+            Console.WriteLine("Project function called");
+        }
+
+        public string GetData(int id)
+        {
+            return $"Data for ID: {id}";
+        }
+
+        public void InitializeDll()
+        {
+            _dllFunctions.SetProjectReference(this);
+        }
+
+        public void CallDllSeparately()
+        {
+            _dllFunctions.DllMethod();
+        }
+    }
+}
