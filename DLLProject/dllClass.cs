61fc35a3e1148bfad533ff04b13c9888ee57cd9e
MaciejM <39690589+siasty@users.noreply.github.com>
2025-03-03 12:13:09 +0100

    Add files via upload

---
+++ DLLProject/dllClass.cs
@@ -0,0 +1,24 @@
+﻿using ClassInterfaces;
+
+namespace DLLProject
+{
+    public class dllClass : IDllFunctions
+    {
+        private IProjectFunctions _projectFunctions;
+
+        public void DllMethod()
+        {
+            Console.WriteLine("DLL method called");
+        }
+
+        public void SetProjectReference(IProjectFunctions projectFunctions)
+        {
+            _projectFunctions = projectFunctions;
+        }
+        public string GetProjectData(int id)
+        {
+            return _projectFunctions?.GetData(id) ?? "No project reference";
+        }
+    }
+
+}
